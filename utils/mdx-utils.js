6495af457c601cb08d624df2901eb8106d213ab5
Leticia-Heeren <leticia.tvsh@gmail.com>
2024-11-16 02:27:19 -0300

    Desafio concluído

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,38 @@
-import { api } from '../services/api'
+import { api } from '../services/api';
 
 export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
+    const { data } = await api.get('/posts'); 
 
-    if(data){
+    if (data) {
         return data;
     }
 
-    return []
-}
+    return [];
+};
 
 export const getPostBySlug = async (id) => {
+    try {
+        // Busca um post específico pelo ID
+        const { data } = await api.get(`/posts?id=eq.${id}`);
+        
+        // Garante que os dados existem e retorna o primeiro item (assumindo que `id` é único)
+        if (data && data.length > 0) {
+            const post = data[0];
+            return {
+                title: post.title,
+                description: post.description,
+                body: post.body,
+                date: post.date,
+                author: post.author,
+                categories: post.categories || [],
+                tags: post.tags || [],
+            };
+        }
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
-    return {}
-}
+        // Retorna vazio se post não foi encontrado
+        return {};
+    } catch (error) {
+        console.error("Erro ao buscar o post:", error);
+        return {};
+    }
+};
